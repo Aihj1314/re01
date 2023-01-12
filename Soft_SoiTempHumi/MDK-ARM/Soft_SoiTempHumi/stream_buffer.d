4ac72d4e178fa4322e3d9882613eddb3769c397e
mayuxin <8403821@qq.com>
2023-01-12 16:25:21 +0800

    模板文件

---
+++ Soft_SoiTempHumi/MDK-ARM/Soft_SoiTempHumi/stream_buffer.d
@@ -0,0 +1,14 @@
+soft_soitemphumi\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c
+soft_soitemphumi\stream_buffer.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+soft_soitemphumi\stream_buffer.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+soft_soitemphumi\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+soft_soitemphumi\stream_buffer.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+soft_soitemphumi\stream_buffer.o: ../Core/Inc/FreeRTOSConfig.h
+soft_soitemphumi\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+soft_soitemphumi\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+soft_soitemphumi\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+soft_soitemphumi\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM0/portmacro.h
+soft_soitemphumi\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+soft_soitemphumi\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+soft_soitemphumi\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+soft_soitemphumi\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/stream_buffer.h
---
+++ Soft_SoiTempHumi/MDK-ARM/Soft_SoiTempHumi/tasks.d
@@ -0,0 +1,16 @@
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/tasks.c
+soft_soitemphumi\tasks.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+soft_soitemphumi\tasks.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+soft_soitemphumi\tasks.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+soft_soitemphumi\tasks.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+soft_soitemphumi\tasks.o: ../Core/Inc/FreeRTOSConfig.h
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM0/portmacro.h
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h
+soft_soitemphumi\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/stack_macros.h
